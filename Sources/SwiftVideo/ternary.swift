67ce18a3580ca059efcd151600a1607d25aed2b1
James Hurley <jamesghurley@gmail.com>
2020-06-24 15:18:12 +0200

    Remove redundant modifier

--- Sources/SwiftVideo/ternary.swift
+++ Sources/SwiftVideo/ternary.swift
@@ -35,7 +35,7 @@ func <|> <I, O>(_ lhs: @escaping (I) -> O,
 
 public extension Collection {
     /// Returns the element at the specified index if it is within bounds, otherwise nil.
-    public subscript (safe index: Index) -> Element? {
+    subscript (safe index: Index) -> Element? {
         return indices.contains(index) ? self[index] : nil
     }
 }
